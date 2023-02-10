0d8c8b414f12c42bbe0b437e22064275a39e5f79
Gabriel Fiami <gabrielfiami.dev@gmail.com>
2023-02-10 18:07:58 -0300

    change color to black on hover

--- script.js
+++ script.js
@@ -13,14 +13,35 @@ inputSize.addEventListener('change', (event) => {
     setGridSize()
 
 })
+//variable for event listener for mouse hover => set color
+let gridPixels;
 
 //set starter gridSize and function to change it from the input range
-const gridElement = `<div class='gridElement'>A</div>`
+const gridElement = `<div class='gridElement'></div>`
+//start screen with default value
 setGridSize()
+
 function setGridSize(){
+    changeGridLayout();
+    addGridElement();
+    }
+
+function changeGridLayout(){
     gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
     gridContainer.innerHTML = '';
+}
+function addGridElement(){
     for(i=0;i<gridSize**2;i++){
         gridContainer.innerHTML += gridElement
-    }
+        addEventListenerToElements();
+
+}}
+
+function addEventListenerToElements(){
+    gridPixels = document.querySelectorAll(".gridElement");
+    gridPixels.forEach((item) => {
+        item.addEventListener('mouseover', (event) => {
+            item.style.backgroundColor = 'black'
+        })
+    } )
 }
